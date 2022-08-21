deeabf41033e8bef2fe9dcc8d87a52a9152e6adf
Tad Lispy <tadeusz@lazurski.pl>
2022-08-21 07:28:25 +0200

    Setup Nix based development environment (#1)
    * Setup Nix based development environment
    
    Including Rust tools and Godot. The .envrc file allows the environment
    to be automatically loaded and unloaded when entering and leaving the
    directory (provided Direnv is installed and integrated with your shell).
    
    All this should not affect you if you are not using Nix and Direnv.
    
    * Enable pure nix builds
    
    With Nix configured to use flakes we can now compile the game in a
    controlled environment by running the following:
    
      nix build
    
    To enable this I took advantage of `bindgenHook` provided by
    `rustPlatform` from `nixpkgs`. This also simplifies the development
    shell setup. No more messing around with strings in the environment. See
    here for more details:
    
        https://github.com/NixOS/nixpkgs/pull/159899
    
    Unfortunately the game compiled with `nix build` doesn't run for me. I'm
    getting the following:
    
        $ result/bin/run_game
        /build/6rs1g5jhs50dzb7mxlfgcbjlkfn9cvl8-source
        thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Os { code: 2, kind: NotFound, message: "No such file or directory" }', run_godot.rs:11:10
        note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    
    I can run without issues with `cargo run` or `./target/release/run_game`:
    
        $ ./target/release/run_game
        /home/tad/Projects/bevy_jam
        Godot Engine v3.4.4.stable.custom_build - https://godotengine.org
        OpenGL ES 2.0 Renderer: AMD RENOIR (LLVM 13.0.1, DRM 3.42, 5.15.59)
        OpenGL ES Batching: ON
    
    It must have something to do with the godot/ directory not being found
    at the expected path. I'm not yet sure how it works, but it seems to me
    that the path is hardcoded during the compilation (see the first line of
    the output). If so, won't this be a problem when we try to distribute
    the binary? Or are we ok with people starting the game with `cargo run`?
    Or am I confused about something (there's always a chance)?

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Bevy Game Jam #2 - Combine <https://itch.io/jam/bevy-jam-2>";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        buildInputs = [
+          pkgs.openssl
+          pkgs.pkgconfig
+          pkgs.rust-bin.stable.latest.default
+          pkgs.clang
+          pkgs.libclang.lib
+          pkgs.godot
+        ];
+        nativeBuildInputs = [
+          pkgs.rustPlatform.bindgenHook
+        ];
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          inherit buildInputs nativeBuildInputs;
+          packages = [ rust-analyzer ];
+        };
+        defaultPackage = rustPlatform.buildRustPackage {
+          name = "bevy-game-jam";
+          version = "1.0.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "bevy_godot-0.2.0" = "sha256-TQl6VleUWgLDSnoqO+PyHB4pUqLGvEP2yw18buelp4A=";
+            };
+          };
+
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
+    );
+}

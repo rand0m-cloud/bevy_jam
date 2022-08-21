d3bc1708bb7161b6308e44b76c7a28d210ba5233
Tad Lispy <tadeusz@lazurski.pl>
2022-08-21 17:47:05 +0200

    WIP: A zombie will follow the player
    There is a problem in my math :-(
    
    Also there is a bit of dead code that is suppose to make each zombie
    roam randomly around. But before I re-enable it I need to get the
    turning logic right.

--- src/zombies.rs
+++ src/zombies.rs
@@ -1,9 +1,13 @@
-use crate::Hp;
+use std::f32::consts::PI;
+
+use crate::{Hp, player::Player};
+use bevy::prelude::*;
 use bevy_godot::prelude::{
     bevy_prelude::{Added, With},
     godot_prelude::Vector2,
     *,
 };
+use rand::prelude::*;
 
 pub struct ZombiesPlugin;
 impl Plugin for ZombiesPlugin {
@@ -17,23 +21,74 @@ impl Plugin for ZombiesPlugin {
 #[derive(Debug, Component)]
 pub struct Zombie;
 
+// A target represents a point where a zombie wants to be
+#[derive(Debug, Component)]
+struct Target(Vector2);
+
+impl Target {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        let distance = rng.gen_range(10.0..100.0);
+        let direction = rng.gen_range(0.0..PI);
+        let vector = Vector2::UP.rotated(direction) * distance;
+        Self(vector)
+    }
+}
+
 fn label_zombies(
     mut commands: Commands,
     entities: Query<(&Groups, Entity), Added<ErasedGodotRef>>,
 ) {
     for (groups, ent) in entities.iter() {
         if groups.is("zombie") {
-            commands.entity(ent).insert(Zombie).insert(Hp(10.0));
+            commands
+                .entity(ent)
+                .insert(Zombie)
+                .insert(Hp(10.0))
+                .insert(Target::random());
         }
     }
 }
 
-fn zombies_move(mut zombies: Query<&mut Transform2D, With<Zombie>>, mut time: SystemDelta) {
+fn zombies_move(
+    mut zombies: Query<(&mut Transform2D, &Target), (With<Zombie>, Without<Player>)>,
+    player: Query<&Transform2D, (With<Player>, Without<Zombie>)>,
+    mut time: SystemDelta,
+) {
     let delta = time.delta_seconds();
-    for mut transform in zombies.iter_mut() {
-        let rotation = transform.rotation();
-        transform.set_rotation(rotation + 0.2 * delta);
-        transform.origin = transform.xform(Vector2::new(0., -1.) * 30.0 * delta);
+    let player = player.single();
+    for (mut zombie, Target(_target)) in zombies.iter_mut() {
+
+        let origin = zombie.origin;
+
+        // Just for testing
+        let target = &player.origin;
+
+        info!("Zombie at {:?} wants to be at {target:?}", zombie.origin);
+
+        // Rotate toward target
+        let heading = (zombie.rotation() / 2.0 / PI);
+        let desired_rotation = (origin.angle_to_point(*target) / 2.0 / PI);
+        let relative_rotation = (desired_rotation - heading);
+
+        info!("Heading:     {heading:?}");
+        info!("Desired:     {desired_rotation:?}");
+        info!("Relative:    {relative_rotation:?}");
+
+
+        let turn = if relative_rotation > 0.0 {
+            info!("Turning right");
+            1.0
+        } else {
+            info!("Turning left");
+            -1.0
+        };
+
+        // let rotation = zombie.rotation();
+        // zombie.set_rotation(rotation + 0.5 * turn * delta);
+
+        // // Move forward
+        // zombie.origin = zombie.xform(Vector2::new(0., 1.) * 30.0 * delta);
     }
 }
 

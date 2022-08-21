be418c0934bfa3adbb6cd4b8b9e73cda9247c12d
Tad Lispy <tadeusz@lazurski.pl>
2022-08-21 10:23:26 +0200

    Zombies will move in circles
    I'm just getting my feet wet with Bevy and Godot.

--- src/zombies.rs
+++ src/zombies.rs
@@ -1,6 +1,7 @@
 use crate::Hp;
 use bevy_godot::prelude::{
     bevy_prelude::{Added, With},
+    godot_prelude::Vector2,
     *,
 };
 
@@ -8,6 +9,7 @@ pub struct ZombiesPlugin;
 impl Plugin for ZombiesPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(label_zombies)
+            .add_system(zombies_move.as_physics_system())
             .add_system(kill_zombies.as_physics_system());
     }
 }
@@ -23,6 +25,15 @@ fn label_zombies(
         if groups.is("zombie") {
             commands.entity(ent).insert(Zombie).insert(Hp(10.0));
         }
+    }
+}
+
+fn zombies_move(mut zombies: Query<&mut Transform2D, With<Zombie>>, mut time: SystemDelta) {
+    let delta = time.delta_seconds();
+    for mut transform in zombies.iter_mut() {
+        let rotation = transform.rotation();
+        transform.set_rotation(rotation + 0.2 * delta);
+        transform.origin = transform.xform(Vector2::new(0., -1.) * 30.0 * delta);
     }
 }
 

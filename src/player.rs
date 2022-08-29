a3f605c7a6a0454ea95dac933b7d6fd7a6d7ef9b
Abby Bryant <rand0m-cloud@outlook.com>
2022-08-28 20:04:34 -0400

    tweak player turning speed

--- src/player.rs
+++ src/player.rs
@@ -14,7 +14,7 @@ use iyes_loopless::prelude::*;
 // TODO: Is there a way to set those in Godot and read them here? It would be nice to be able to experiment with constants on the fly.
 const WALKING_SPEED: f32 = 70.0;
 const RUNNING_SPEED: f32 = 150.0;
-const TURNING_SPEED: f64 = 4.0;
+const TURNING_SPEED: f64 = 9.0;
 
 pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
